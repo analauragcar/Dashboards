4e1903134e7eb0b77339897c26520c98c9e03ff5
analauragcar <148863673+analauragcar@users.noreply.github.com>
2024-08-19 20:33:29 -0300

    venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\13528460474\Documents\GitHub\Dashboards\.venv
+set VIRTUAL_ENV=c:\Users\anala\OneDrive\Documentos\GitHub\Dashboards\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
-import pandas as pd
 
-
+st.header('Dashboard')
+st.text('Texto')
+st.sidebar.text("Filtrar Dados")
